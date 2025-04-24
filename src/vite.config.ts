edbc77b2fa678666477669055aac0bdb8e859adf
nor0x <nor0x@users.noreply.github.com>
2025-04-24 16:23:01 +0200

    title

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/Typograph/', // Replace <repository-name> with your GitHub repo name
+  base: '/', // Replace <repository-name> with your GitHub repo name
 });
